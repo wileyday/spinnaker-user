84464aca226f54475122e72505973e15caf3cd44
Wiley <wiley@nurigo.net>
2017-10-17 16:47:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ app.get('/user', function (req, res) {
   res.send('Hello User!!!');
 });
 
+/*
 app.get('/healthcheck', function (req, res) {
   res.send('checking health... OK!');
 });
-
+*/
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
