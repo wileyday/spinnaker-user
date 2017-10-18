5f7c28069b0eb61c88e87d713394ba27389b3205
Wiley <wiley@nurigo.net>
2017-10-18 16:26:08 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello User2#####');
-});
-
 app.get('/user', function (req, res) {
   res.send('Hello User!!!');
 });
 
-/*
 app.get('/healthcheck', function (req, res) {
   res.send('checking health... OK!');
 });
-*/
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
